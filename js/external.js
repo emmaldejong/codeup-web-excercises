04a2749664ae04cd946f309dbd73faaa4995f810
emmadejong <dejong.l.emma@gmail.com>
2018-01-25 16:23:37 -0600

    added js files from exercise with html

---
+++ js/external.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log('Hello from external JavaScript!');
+var colorOfWhale = prompt('What is the color of a Blue Whale?');
+console.log('The color of a Blue Whale is' + ' ' + colorOfWhale);
+alert('Thank you for giving the correct answer, YOUR A WINNER!!!!');
