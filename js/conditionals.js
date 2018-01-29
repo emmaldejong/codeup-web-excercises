07938655ccee49826ec7cf95f73f3d7426d33ce7
emmadejong <dejong.l.emma@gmail.com>
2018-01-29 16:57:14 -0600

    added review comments & started bonus #2

--- js/conditionals.js
+++ js/conditionals.js
@@ -201,9 +201,35 @@ function dayofTheWeekStartsWithT(dayName) {
             return false;
     } else if (dayName === 'Saturday'){
         return false;
-    } else if (dayName === 'Sunday') {i
+    } else if (dayName === 'Sunday') {
         return false;
     }
 }
 console.log(randomDay);
 console.log(dayofTheWeekStartsWithT(randomDay));
+
+/** Bonus 2. DONE WITH RYAN
+ Write a function, isValidPassword, that takes in a string argument and returns true or false
+ depending on whether or not all the following conditions are true:
+ 1) Must be 6 characters long (only for the sake of this exercise, NOT a best practice)
+ 2) Contains at least one letter and one number
+ 3) Contains at least one upper and one lower case letter
+ 4) Only comprised of letters and numbers
+ 5) EXTRA BONUS: is not the same forwards and backwards
+
+ **/
+function isValidPassword(input) {
+    var lengthIsGood = input.length == 6;
+    var hasAtLeastOneLetter;
+    var hasAtLeastOneNumber;
+    var hasLowerCaseLetter;
+    var hasUpperCaseLetter;
+    var alphaNumeric;
+
+    return lengthIsGood && hasAtLeastOneLetter && hasAtLeastOneNumber && hasLowerCaseLetter&& hasUpperCaseLetter && alphaNumeric;
+}
+function hasLetter(input) {
+    input = input.toLowerCase();
+
+    return input.includes('a') || input.includes('b') || input.includes('c')...
+}
