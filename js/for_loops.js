aed4d9d021cb5f959f06a0904600393ed5151d5f
emmadejong <dejong.l.emma@gmail.com>
2018-01-30 12:30:03 -0600

    created for loops file for exercise, not yet completed

---
+++ js/for_loops.js
@@ -0,0 +1,9 @@
+"use strict";
+var i = 0;
+function showMultiplicationTable(n) {
+
+}
+console.log(showMultiplicationTable(2));
+
+var randomNum = Math.floor(Math.random() * 200) + 20;
+console.log(randomNum);
