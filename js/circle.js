b788e5f7fa9367364a7acc92854501a7d8859bcb
emmadejong <dejong.l.emma@gmail.com>
2018-02-02 11:06:43 -0600

    completed circle.js exercise

--- js/circle.js
+++ js/circle.js
@@ -1,39 +1,42 @@
 (function() {
     "use strict";
-
     // create a circle object
     var circle = {
         radius: 3,
-
-        getArea: function () {
+        getArea: function() {
             // TODO: complete this method
+            //area of a circle = pi*r ^ 2
             var area = Math.PI * Math.pow(this.radius, 2);
-
-            return area; // TODO: return the proper value
+            // TODO: return the proper value
+            return area;
         },
-
-        logInfo: function (doRounding) {
+        logInfo: function(doRounding) {
             // TODO: complete this method.
-
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            var circleArea;
+            if(doRounding) {
+                circleArea = Math.round(this.getArea());
+            } else {
+                circleArea = this.getArea();
+            }
+            console.log("Area of a Circle with Radius: " + this.radius + ", is: " + circleArea);
         }
     };
 
     // log info about the circle
-    console.log("Raw circle information");
+    console.log("Raw Circle Information:");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+    console.log("Circle Information Rounded to the Nearest Whole Number");
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
-    console.log("Raw circle information");
+    console.log("Raw Circle Information:");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+    console.log("Circle Information Rounded to the Nearest Whole Number");
     circle.logInfo(true);
 })();
