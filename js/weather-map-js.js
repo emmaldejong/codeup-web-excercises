ce3f41a476ef87ca7a6fc2e3281f8987f06c2fef
emmadejong <dejong.l.emma@gmail.com>
2018-02-17 10:07:33 -0600

    start external file use and refactoring of code & added background image to page

---
+++ js/weather-map-js.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('hello');
+
+
+
+    // var ajaxForecast = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+    //     APPID: "7f8e3aa0aad113510e0c1eaafd1c17b8",
+    //     lat: $('#lat').val(),
+    //     lon: $('#long').val(),
+    //     units: "imperial",
+    //     cnt: 3
+    // });
+    // ajaxForecast.done(function (response) {
+    //     console.log(response);
+    // })
