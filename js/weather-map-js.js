2b2eb111e2b382ed097d2ce8e3a441170056e625
emmadejong <dejong.l.emma@gmail.com>
2018-02-20 09:58:11 -0600

    refactioring changes minor still in progress

--- js/weather-map-js.js
+++ js/weather-map-js.js
@@ -1,16 +1,28 @@
-"use strict";
+(function () {
+    "use strict";
+    
+    console.log('hello');
 
-console.log('hello');
+    var ajaxForecast = $.get("http://api.openweathermap.org/data/2.5/forecast?id=4726206", {
+        APPID: "2c2d07a95167bb29b02bf086b161b7d8",
+        id:     4726206,
+        // lat: $('#lat').val(),
+        // lon: $('#long').val(),
+        units: "imperial",
+        cnt: 3
+    });
+    ajaxForecast.done(function (response) {
+        console.log(response);});
+        
+    function buildWeatherHTML() {
+        var weatherHTML = '';
+        for (var i = 0; i <= i; i++) {
+            weatherHTML += "<div>";
+            weatherHTML += "<div>";
+
+        }
+
+    }        
 
 
-
-    // var ajaxForecast = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-    //     APPID: "7f8e3aa0aad113510e0c1eaafd1c17b8",
-    //     lat: $('#lat').val(),
-    //     lon: $('#long').val(),
-    //     units: "imperial",
-    //     cnt: 3
-    // });
-    // ajaxForecast.done(function (response) {
-    //     console.log(response);
-    // })
+}());
