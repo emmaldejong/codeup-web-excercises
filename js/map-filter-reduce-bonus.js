16a91369cd9c19bd5585ecd920fb47d52c121de2
emmadejong <dejong.l.emma@gmail.com>
2018-02-21 12:32:02 -0600

    finished a little more bonuses and marked the ones that are unfinished

--- js/map-filter-reduce-bonus.js
+++ js/map-filter-reduce-bonus.js
@@ -22,80 +22,115 @@ const customers = [
 ];
 // PROBLEM 1 - create an array of the first letters of each fruit
 // PROBLEM 1 HINT - use .map()
-const firstLetterOfFruit = fruits.map(function() {
-    fruits.forEach(function (fruit) {
-        return fruit.charAt(0);
-    })
-});
+const firstLetterOfFruit = fruits.map(fruit => fruit.charAt(0));
 console.log(firstLetterOfFruit);
 
 // PROBLEM 2 - create array of user objects based on the customers array
 // of objects (each user object should just have name and age properties)
 // PROBLEM 2 HINT - use .map()
-const customerArray = customers.map(function() {
-    let newArray = [];
-    customers.forEach(function () {
-        newArray.push(customers);
-    })
+
+const customerArray = customers.map(customer => {
+        return {name: customer.name, age: customer.age}
 });
 console.log(customerArray);
 
 // PROBLEM 3 - create an array of civil servant customers (teachers and police officers)
 // containing the same properties as the objects on the customers objects
 // PROBLEM 3 HINT - use .filter()
-const civilServants = customers.filter(function(n) {
-    return customers.occupation === 'Police Officer' || customers.occupation === 'Teacher';
+const civilServants = customers.filter(function(customer) {
+    return customer.occupation === 'Police Officer' || customer.occupation === 'Teacher';
 });
 console.log(civilServants);
 
 
 // PROBLEM 4 - determine the average age of customers
 // PROBLEM 4 HINT - use .reduce()
-const avgAge = customers.reduce((total, age) => total + age) / customers.length;
+const avgAge = customers.reduce((total, customer) => {
+    return total + customer.age / customers.length;
+}, 0);
 console.log(avgAge);
 
 //MORE BONUSES
 // Given the following array...
-// const names = ["John", "Max", "Ronald"];
+const names = ["John", "Max", "Ronald"];
 // complete the bonuses below...
 // - Create an array where all names are given a last name of Smith.
+//TODO
 // - Create an array where each word is in all caps
+const allCaps = names.map(name => name.toUpperCase());
+console.log(allCaps);
 // - Create an array where all names have more than 3 letters
+const threeLetters = names.filter(name => name.length > 3);
+console.log(threeLetters);
 // - Create an array of names with only the last two letters of each name
+//TODO
 // - Create a total count of all letters
+//TODO
 // - Create a string of all letters in alphabetical order
+//TODO
 // - Create an array of word objects with properties of wordLength, firstLetter, lastLetter
+//TODO
 // - Create a string of all vowels in the entire array of names
+//TODO
 // - Create a single object with properties
+//TODO
 
 
 
 //EVEN MORE BONUSES
 // Given the following array...
-// const family = [
-//     {
-//         name: "Karen",
-//         gender: "female",
-//         age: 29,
-//     },
-//     {
-//         name: "Summer",
-//         gender: "female",
-//         age: 10,
-//     },
-//     {
-//         name: "Bob",
-//         gender: "male",
-//         age: 32,
-//     },
-// ];
+const family = [
+    {
+        name: "Karen",
+        gender: "female",
+        age: 29,
+    },
+    {
+        name: "Summer",
+        gender: "female",
+        age: 10,
+    },
+    {
+        name: "Bob",
+        gender: "male",
+        age: 32,
+    },
+];
 // complete the bonuses below...
 // - Calculate the average age of family members
+const averageAge = family.reduce((total, person) => {
+    return total + person.age/family.length;
+}, 0);
+console.log(averageAge);
+
 // - Create an array of family objects without the age property
+// TODO
+const ageNone = family.filter(function (element) {
+    return element != element.age;
+});
+console.log(ageNone);
+
 // - Create an array of all minors
+const minorAge = family.filter(function (age) {
+    return age.age >= 18;
+});
+console.log(minorAge);
+
 // - Calculate the total age combined of family members
+const totalAge = family.reduce((total, person) => {
+    return total + person.age;
+}, 0);
+console.log(totalAge);
+
 // - Create an array of only female family member objects
+const onlyFemale = family.filter(function (familyMember) {
+   return familyMember.gender === 'female';
+});
+console.log(onlyFemale);
+
 // - Create a single object with properties containing arrays of all names, genders, and ages
-
-
-
+// TODO
+const familyObject = family.reduce((accum, person) => {
+    return accum[person] = person;
+});
+console.log(familyObject);
