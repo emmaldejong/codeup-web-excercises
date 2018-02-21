501c6a0db86933f6b410581b22faaf2d40d6db23
emmadejong <dejong.l.emma@gmail.com>
2018-02-21 09:12:03 -0600

    initial commit for bonus problems

---
+++ js/map-filter-reduce-bonus.js
@@ -0,0 +1,34 @@
+const fruits = ["cantaloupe", "orange", "date", "elderberry", "ugli fruit", "pineapple"];
+const customers = [
+    {
+        name: "Fred",
+        age: 58,
+        occupation: "Police Officer",
+        noOfPurchases: 4
+    },
+    {
+        name: "Samantha",
+        age: 54,
+        occupation: "Teacher",
+        noOfPurchases: 18
+    },
+    {
+        name: "Charles",
+        age: 38,
+        occupation: "Librarian",
+        noOfPurchases: 9
+    }
+];
+// PROBLEM 1 - create an array of the first letters of each fruit
+
+// PROBLEM 2 - create array of user objects based on the customers array
+// of objects (each user object should just have name and age properties)
+
+// PROBLEM 3 - create an array of civil servant customers (teachers and police officers)
+// containing the same properties as the objects on the customers objects
+// PROBLEM 4 - determine the average age of customers
+
+//            PROBLEM 1 HINT - use .map()
+//            PROBLEM 2 HINT - use .map()
+//            PROBLEM 3 HINT - use .filter()
+//            PROBLEM 4 HINT - use .reduce()
