6ca8de7d0dd942e13364970d86991d6267490f96
emmadejong <dejong.l.emma@gmail.com>
2018-02-22 09:06:58 -0600

    added Zachs solution to the second problem

--- js/promises.js
+++ js/promises.js
@@ -1,31 +1,43 @@
 "use strict";
-console.info("WAIT FUNCTION #1");
 // Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-const wait = delay => {
+const wait = (cactus) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(delay);
-            }, delay);
+            resolve(cactus);
+            }, cactus);
         });
 };
 
-wait(3000).then((delay) => console.log(`Wait finished after ${delay / 1000} seconds!`));
+wait(2000).then((cactus) => console.log(`Wait finished after ${cactus / 1000} seconds!`));
 
-// console.info("GITHUB COMMIT CHECK #2");
 // Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
 
+// const lastCommit = username => {
+//     fetch(`https://api.github.com/users/${username}/repos`,
+//         {headers: {'Authorization': '1726fc6dd8b4516b05ae18bd9eb792a862296946'}})
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// };
+// lastCommit("emmaldejong");
+
 const lastCommit = username => {
-    const url = `https://api.github.com/users/${username}/events/public`;
-    const options = {
-        headers: {
-            'Authorization': '5e486985d84767d4d688aa9eb55b0842546e34f4'
-        }
-    };
-    return fetch(url, options)
+    return fetch(`https://api.github.com/users/${username}/events`,
+        {headers: {'Authorization': '1726fc6dd8b4516b05ae18bd9eb792a862296946'}})
         .then(response => response.json())
-        .then(data => data[0].created_at);
+        .then(events => events[0].created_at);
 };
-console.log(lastCommit('emmaldejong').then(lastCommitDate => console.log(lastCommitDate)));
+
+const users = [
+    {name: 'Emma', username: 'emmaldejong'},
+    {name: 'Zach', username: 'zgulde'},
+    {name: 'Ryan', username: 'ryanorsinger'}
+];
+
+users.forEach(user => {
+    lastCommit(user.username).then(lastCommitDate => {
+        console.log(`${user.name}'s last commit was ${lastCommitDate}`);
+    });
+});
