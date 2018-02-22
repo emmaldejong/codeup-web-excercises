febfea6b7e99173121f8e6693ffebe607ca872b2
emmadejong <dejong.l.emma@gmail.com>
2018-02-22 11:05:14 -0600

    added walk thru notes and jordan explanation

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,28 @@
 "use strict";
-// Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+//###################################################################################################################
+// 1. Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 const wait = (cactus) => {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(cactus);
+            resolve(`Wait finished after ${cactus / 1000} seconds!`);
             }, cactus);
         });
 };
+//wait is the function and cactus is whatever you are going to pass through that function
+//you don't need any argument parameter in the setTimeout() because we pass when it should stop thru cactus
 
-wait(2000).then((cactus) => console.log(`Wait finished after ${cactus / 1000} seconds!`));
 
-// Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
+wait(2000).then((data) => console.log(data));
+wait(3400).then((data) => console.log(data));
+wait(4600).then((data) => console.log(data));
 
+//###################################################################################################################
+
+// 2. Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
+//DON'T PUSH YOUR KEY TO GITHUB
 // const lastCommit = username => {
 //     fetch(`https://api.github.com/users/${username}/repos`,
 //         {headers: {'Authorization': '1726fc6dd8b4516b05ae18bd9eb792a862296946'}})
@@ -41,3 +49,30 @@ users.forEach(user => {
         console.log(`${user.name}'s last commit was ${lastCommitDate}`);
     });
 });
+
+//JORDAN'S EXPLANATION OF PROMISES
+//synchronous code
+// fetch(make a call to a server in china)
+//     .....call takes like 10 seconds
+// function LoadOurPage() {}
+//notes: this page takes forever to load and sucks for the user
+
+//asynchronous code
+// new Promise(ok, i'll start a request to the server in china') {
+//     look, it's going to take a while'
+//     so go ahead and run the rest of the code
+//     i promise when i connect to the API i'll give you the code you need'
+//}).then( (the data i promised you) => console.log(here you go!};
+
+//now we can skip to...
+
+//function LoadOurPage() {}
+
+//notes: this page loads much better because we can run the rest of the code while we are connecting to the server
+//Promises let the code continue while the data is being fetched
+//so you can think of Promises as an IOU
+
+
+
+
+
